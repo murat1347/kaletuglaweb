e6fde2b66e46cf599a2cf179078530a385e37479
murat çiçek <murat1347@yandex.com>
2019-12-13 19:20:47 +0300

    ‘theend’

--- article/forms.py
+++ article/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': "form-control"}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': "form-control"}))
-    title=forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': "form-control"}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control'}), required=True)
+    name = forms.CharField(max_length=50,label="Adınız Soyadınız ",widget=forms.TextInput(attrs={'class': "form-control"}))
+    email = forms.EmailField(label="E-Posta Adresiniz",widget=forms.TextInput(attrs={'class': "form-control"}))
+    title = forms.CharField(label="Konu",max_length=50,widget=forms.TextInput(attrs={'class': "form-control"}))
+    message = forms.CharField(label="Mesajınız",widget=forms.Textarea(attrs={'class' : 'form-control'}), required=True)
